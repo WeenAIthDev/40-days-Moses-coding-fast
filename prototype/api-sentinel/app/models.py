f97a64b197d818b64b675d393f8041b451cceaa2
Technodox10 <rafeliaedwigefernandes97@gmail.com>
2026-09-07 00:00:40 +0530

    relationship() understanding

--- prototype/api-sentinel/app/models.py
+++ prototype/api-sentinel/app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import ForeignKey
 from datetime import datetime, timezone
+from sqlalchemy.orm import relationship
 class Base(DeclarativeBase):
     pass
 
